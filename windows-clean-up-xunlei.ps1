2653c55496b14a86817959c82237503e5ff168a2
yaofei zheng <zyf26256@126.com>
2026-09-04 19:51:21 +0800

    windows-clean-up-xunlei.ps1: add more clean cmd

--- windows-clean-up-xunlei.ps1
+++ windows-clean-up-xunlei.ps1
@@ -1,1 +1,5 @@
 Get-AppxPackage -Name 'ThunderShell' | Remove-AppxPackage
+Stop-Service -Name "XLServicePlatform"
+Set-Service -Name "XLServicePlatform" -StartupType Manual
+Remove-Item "$env:APPDATA\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\迅雷.lnk"
+Remove-Item "$env:USERPROFILE\Desktop\迅雷.lnk"

6621d20959cd8ed743abf810bd8329065d00b2ef
yaofei zheng <zyf26256@126.com>
2026-09-04 20:10:02 +0800

    windows-clean-up-douyin.ps1: add DouyinElevationService

--- windows-clean-up-douyin.ps1
+++ windows-clean-up-douyin.ps1
@@ -1,2 +1,4 @@
 Stop-Service -Name "douyin_performance_service"
 Set-Service -Name "douyin_performance_service" -StartupType Manual
+Stop-Service -Name "DouyinElevationService"
+Set-Service -Name "DouyinElevationService" -StartupType Manual
